d7ca282617db17771e4dcd9b907bafabbe7170e5
Miguel Madrid <angelmadrid.master@gmail.com>
2026-09-08 09:58:42 -0500

    Agregando las clases, main y SumarNumeros

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        SumarNumeros obj = new SumarNumeros();
+        int numA,numB,numC;
+        System.out.println("Ingrese el valor del numero A:");
+        numA=Integer.parseInt(br.readLine());
+        System.out.println("Ingrese el valor del numero B:");
+        numB=Integer.parseInt(br.readLine());
+        System.out.println("Ingrese el valor del numero C:");
+        numC=Integer.parseInt(br.readLine());
+        obj.setNumA(numA);
+        obj.setNumB(numB);
+        obj.setNumC(numC);
+        System.out.println("EL valor del resultado de la suma es: "+obj.obtenerSuma());
+    }
+}
---
+++ src/SumarNumeros.java
@@ -0,0 +1,33 @@
+public class SumarNumeros {
+    private int numA;
+    private int numB;
+    private int numC;
+
+    public int getNumA() {
+        return numA;
+    }
+
+    public void setNumA(int numA) {
+        this.numA = numA;
+    }
+
+    public int getNumB() {
+        return numB;
+    }
+
+    public void setNumB(int numB) {
+        this.numB = numB;
+    }
+
+    public int getNumC() {
+        return numC;
+    }
+
+    public void setNumC(int numC) {
+        this.numC = numC;
+    }
+
+    public int obtenerSuma(){
+        return numA+numB+numC;
+    }
+}
